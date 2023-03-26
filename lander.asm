9e03eb03096546638a26bce1a6716643b1055614
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2023-03-26 14:57:40 +0100

    Update lander.asm

--- lander.asm
+++ lander.asm
@@ -25,12 +25,14 @@
 #define KEYBOARD_READ_PORT_SPACE_TO_B $7F 
 ; starting port numbner for keyboard, is same as first port for shift to v
 #define KEYBOARD_READ_PORT $FE 
-
-; LEM is the Apollo Lunar Excursion Module
-#define LEM_CHARACTER 187
-
 #define SCREEN_WIDTH 32
 #define SCREEN_HEIGHT 23   ; we can use the full screen becuase we're not using PRINT or PRINT AT ROM subroutines
+
+#define NON_FIRED 0
+#define MAIN_ENGINE 1
+#define LEFT_THRUSTER 2
+#define RIGHT_THRUSTER 3
+#define GRAVITY 1
 
 ; character set definition/helpers
 __:				EQU	$00	;spacja
@@ -170,25 +172,38 @@ gameLoop
     ld a, KEYBOARD_READ_PORT_SHIFT_TO_V			
     in a, (KEYBOARD_READ_PORT)					; read from io port	
     bit 1, a                            ; Z
-    jp z, drawLeft
+    jp z, leftThruster
 
     ld a, KEYBOARD_READ_PORT_SPACE_TO_B			
     in a, (KEYBOARD_READ_PORT)					; read from io port		
     bit 2, a						    ; M
-    jp z, drawRight							    ; jump to move shape right	
+    jp z, rightThruster							    ; jump to move shape right	
 
     ld a, KEYBOARD_READ_PORT_SPACE_TO_B			
     in a, (KEYBOARD_READ_PORT)					; read from io port		
     bit 3, a					        ; N
-    jp z, drawDown
+    jp z, thrustMainEngine
+    
+    ld d, NON_FIRED
+    ld e, 0
+    jr updateStateAndDrawLEM
 
-drawLeft 
-    call drawLEM        
-drawRight
-    call drawLEM        
-drawUp
-    call drawLEM        
-drawDown
+leftThruster
+    ld d,1    ; d storing the thrust 
+    ld e,LEFT_THRUSTER     ; e storing which engine or thruster
+    jr updateStateAndDrawLEM    
+rightThruster    
+    ld d,1    ; d storing the thrust 
+    ld e,RIGHT_THRUSTER     ; e storing which engine or thruster
+    jr updateStateAndDrawLEM
+thrustMainEngine
+    ld d,3    ; d storing the thrust 
+    ld e,MAIN_ENGINE     ; e storing which engine or thruster
+    jr updateStateAndDrawLEM
+    ;;;;;;;;; NO CODE SHOULD GO BETWEEN THIS AND  call updateLEMPhysicsState
+    
+updateStateAndDrawLEM    
+    call updateLEMPhysicsState
     call drawLEM            
     
     call waitLoop
@@ -240,11 +255,78 @@ waitPlayerWon
     jp initVariables
     ;; never gets to here
 
- 
+
+updateLEMPhysicsState
+
+                        ; d stores thruster, e stores the thrust to apply set by caller
+    ld a, d
+    cp MAIN_ENGINE
+    jp z, mainEngine    
+    ;cp LEFT_THRUSTER
+    ;jp z, leftThrust    
+    ;cp RIGHT_THRUSTER
+    ;jp z, rightThrust
+    ;cp NON_FIRED
+    jp calculateNewPosition  
+    
+leftThrust
+    ld a, (x_vel)
+    dec a
+    ld (x_vel), a
+rightThrust
+    ld a, (x_vel)
+    inc a
+    ld (x_vel), a
+mainEngine
+    ld a, (y_vel)
+    add a, e
+    ld (y_vel), a
+    
+calculateNewPosition  
+    ; always subtract the gravity until max v reached
+    ld a, (y_vel)
+    ;; need to check if a is > zero
+    ld b, 1
+    
+    add a, b
+    ld (y_vel), a
+    
+    ;ld hl, (playerPosAbsolute)
+    ;ld d, 0
+    ;ld e, (x_vel)
+    ;ld a, 128
+    ;cp e
+    ;jp c, goingLeft
+    ;jp goingRight
+    jp addVertical
+    
+goingLeft    
+    sbc hl, de 
+    jp addVertical
+goingRight
+    add hl, de
+    jp addVertical
+    ld (playerPosAbsolute), hl
+        
+addVertical    
+    ld hl, (playerPosAbsolute)
+    ld de, $0021
+    ld a, (y_vel)  
+    ld c, 0
+addVerticalLoop    
+    add hl, de    
+    dec a
+    cp a
+    jr nz,  addVerticalLoop
+    
+    ld (playerPosAbsolute), hl
+    
+endOfUpdatePhysics
+    ret
     
 waitLoop
 #ifdef RUN_ON_EMULATOR
-    ld bc, $0fff     ; set wait loop delay for emulator
+    ld bc, $2fff     ; set wait loop delay for emulator
 #else
     ld bc, $0eff     ; set wait loop delay 
 #endif    
@@ -384,22 +466,28 @@ blankText
     DEFB	__,__,__,__,__,__,__,__,$ff    
 tempChar
     DEFB 0
-mazeDrawAbasolute    
-    DEFB 0
 playerPosAbsolute
     DEFB 0,0
 playerRowPosition
     DEFB 0
 playerColPosition
     DEFB 0
-firstCharFirstRow
-    DEFB 0,0
-lastCharFirstRow    
-    DEFB 0,0
 firstTime    
     DEFB 1
 Score    
     DEFB 0
+
+
+;;;;; LEM state
+x_vel
+    DEFB 0
+pad1    
+    DEFB 0                
+y_vel
+    DEFB 0            
+pad2 
+    DEFB 0            
+    
 VariablesEnd:   DEFB $80
 BasicEnd: 
 #END
